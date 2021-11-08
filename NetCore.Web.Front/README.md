5567045d586b4a95bfab20d24c8ca4ae8845ebce
Franco Roberto Maieli <maieli.franco@gmail.com>
2021-11-08 16:57:32 -0300

    Deleted NetCore.Web.Front from solution

--- NetCore.Web.Front/README.md
+++
@@ -1,3 +0,0 @@
-# NetCore.Web.Front
-
-
--- NetCore.Web.Front/setupEnzyme.ts
+++
@@ -1,4 +0,0 @@
-import { configure } from 'enzyme';
-import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-configure({ adapter: new EnzymeAdapter() });
